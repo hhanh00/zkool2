f02ddf3c4b5e2dcfbfe1f5d7938c3952a10a3fe1
hhanh00 <hanh425@gmail.com>
2025-04-08 23:53:32 +0800

    feat: add fees manager (#42)

--- rust/src/pay/mod.rs
+++ rust/src/pay/mod.rs
@@ -1,6 +1,10 @@
+use self::error::Result;
+use pool::PoolMask;
+
 pub mod error;
-pub mod pool;
+pub mod fee;
 pub mod plan;
+pub mod pool;
 pub mod prepare;
 
 pub struct Recipient {
@@ -9,4 +13,26 @@ pub struct Recipient {
     pub pools: Option<u8>,
     pub user_memo: Option<String>,
     pub memo_bytes: Option<Vec<u8>>,
+}
+
+pub struct RecipientState {
+    pub recipient: Recipient,
+    pub remaining: u64,
+    pub pool_mask: PoolMask,
+}
+
+impl RecipientState {
+    pub fn new(recipient: Recipient) -> Result<Self> {
+        let amount = recipient.amount;
+        let pool_mask = PoolMask::from_address(&recipient.address)?;
+        Ok(Self {
+            recipient,
+            remaining: amount,
+            pool_mask,
+        })
+    }
+
+    pub fn to_inner(self) -> Recipient {
+        self.recipient
+    }
 }
