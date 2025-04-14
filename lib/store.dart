dd774e9caa935645b93621aa2b82cfea2859dddb
hhanh00 <hanh425@gmail.com>
2025-04-14 08:22:21 +0800

    feat: synchronization retry with exponential backoff (#80)

--- lib/store.dart
+++ lib/store.dart
@@ -1,17 +1,26 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:mobx/mobx.dart';
 import 'package:zkool/main.dart';
 import 'package:zkool/src/rust/api/account.dart';
 import 'package:zkool/src/rust/api/init.dart';
+import 'package:zkool/src/rust/api/network.dart';
+import 'package:zkool/src/rust/api/sync.dart';
 
 part 'store.g.dart';
 
 class AppStore = AppStoreBase with _$AppStore;
 
 abstract class AppStoreBase with Store {
-  @observable String accountName = "";
-  @observable List<Account> accounts = [];
-  @observable List<Tx> transactions = [];
-  @observable List<Memo> memos = [];
+  @observable
+  String accountName = "";
+  @observable
+  List<Account> accounts = [];
+  @observable
+  List<Tx> transactions = [];
+  @observable
+  List<Memo> memos = [];
 
   bool includeHidden = false;
   ObservableList<String> log = ObservableList.of([]);
@@ -38,6 +47,38 @@ abstract class AppStoreBase with Store {
   Future<void> loadMemos() async {
     final mems = await listMemos();
     memos = mems;
+  }
+
+  bool syncInProgress = false;
+  int retryCount = 0;
+  Timer? retrySyncTimer;
+
+  Future<Stream<SyncProgress>> startSynchronize(List<int> accounts) async {
+    if (syncInProgress) {
+      return Stream.empty();
+    }
+
+    syncInProgress = true;
+    retrySyncTimer?.cancel();
+    retrySyncTimer = null;
+    final currentHeight = await getCurrentHeight();
+    final progress =
+        synchronize(accounts: accounts, currentHeight: currentHeight)
+            .asBroadcastStream();
+    progress.listen((_) => retryCount = 0, onError: (_) {
+      syncInProgress = false;
+      retryCount++;
+      final maxDelay = pow(2, min(retryCount, 10)).toInt(); // up to 1024s = 17min
+      final delay = Random().nextInt(maxDelay); // randomize delay
+      logger.i("Sync error, retrying in $delay seconds");
+      retrySyncTimer?.cancel();
+      retrySyncTimer = Timer(Duration(seconds: delay), () {
+        startSynchronize(accounts);
+      });
+    }, onDone: () {
+      syncInProgress = false;
+    });
+    return progress;
   }
 
   static AppStore instance = AppStore();
