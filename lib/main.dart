437caa71df7b082ca0e1aa2440b992acb783f725
hhanh00 <hanh425@gmail.com>
2025-04-18 00:21:51 +0800

    fix: new account by xprv/xpub/bip38 (#103)

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toastification/toastification.dart';
 import 'package:zkool/router.dart';
-import 'package:zkool/src/rust/api/network.dart';
 import 'package:zkool/src/rust/frb_generated.dart';
 import 'package:zkool/store.dart';
 
