dd5acb8eb0d53335d052a3121e7db0d34ce797b1
hhanh00 <hanh425@gmail.com>
2025-04-21 17:10:13 +0800

    feat: add tutorial (#122)

--- lib/pages/new_account.dart
+++ lib/pages/new_account.dart
@@ -8,11 +8,24 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:showcaseview/showcaseview.dart';
 import 'package:zkool/src/rust/api/account.dart';
 import 'package:zkool/src/rust/api/key.dart';
 import 'package:zkool/store.dart';
 import 'package:zkool/utils.dart';
 import 'package:zkool/validators.dart';
+
+final importID = GlobalKey();
+final saveID = GlobalKey();
+final iconID = GlobalKey();
+final nameID = GlobalKey();
+final internalID = GlobalKey();
+final restoreID = GlobalKey();
+
+final keyID = GlobalKey();
+final passphraseID = GlobalKey();
+final accountIndexID = GlobalKey();
+final birthID = GlobalKey();
 
 class NewAccountPage extends StatefulWidget {
   const NewAccountPage({super.key});
@@ -25,23 +38,45 @@ class NewAccountPageState extends State<NewAccountPage> {
   var name = "";
   var restore = false;
   var key = "";
+  var isSeed = false;
   Uint8List? iconBytes;
   final formKey = GlobalKey<FormBuilderState>();
 
+  void tutorial() async {
+    tutorialHelper(context, "tutNew0", [
+      nameID, iconID, internalID, restoreID, importID, saveID
+    ]);
+    if (restore) tutorialHelper(context, "tutNew1", [
+      keyID, birthID
+    ]);
+    if (restore && isSeed) tutorialHelper(context, "tutNew2", [
+      passphraseID, accountIndexID
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Future(tutorial);
+
     final ib = iconBytes;
-    final isSeed = isValidPhrase(phrase: key);
+    isSeed = isValidPhrase(phrase: key);
 
     return Scaffold(
         appBar: AppBar(
           title: const Text("New Account"),
           actions: [
-            IconButton(onPressed: onImport, icon: Icon(Icons.file_download)),
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: onSave,
-            ),
+            Showcase(
+                key: importID,
+                description: "Import an account from file",
+                child: IconButton(
+                    onPressed: onImport, icon: Icon(Icons.file_download))),
+            Showcase(
+                key: saveID,
+                description: "Save",
+                child: IconButton(
+                  icon: const Icon(Icons.save),
+                  onPressed: onSave,
+                )),
           ],
         ),
         body: Padding(
@@ -52,12 +87,15 @@ class NewAccountPageState extends State<NewAccountPage> {
                 child: Column(
                   children: [
                     Stack(children: [
-                      CircleAvatar(
-                        radius: 60,
-                        backgroundImage:
-                            ib != null ? Image.memory(ib).image : null,
-                        child: ib == null ? Text(initials(name)) : null,
-                      ),
+                      Showcase(
+                          key: iconID,
+                          description: "Upload a icon",
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundImage:
+                                ib != null ? Image.memory(ib).image : null,
+                            child: ib == null ? Text(initials(name)) : null,
+                          )),
                       Positioned(
                           right: 0,
                           bottom: 0,
@@ -67,56 +105,74 @@ class NewAccountPageState extends State<NewAccountPage> {
                           ))
                     ]),
                     Gap(16),
-                    FormBuilderTextField(
-                      name: "name",
-                      decoration:
-                          const InputDecoration(labelText: "Account Name"),
-                      initialValue: name,
-                      onChanged: (v) => setState(() => name = v!),
-                    ),
+                    Showcase(
+                        key: nameID,
+                        description:
+                            "Enter a name that identifies this account",
+                        child: FormBuilderTextField(
+                          name: "name",
+                          decoration:
+                              const InputDecoration(labelText: "Account Name"),
+                          initialValue: name,
+                          onChanged: (v) => setState(() => name = v!),
+                        )),
                     Gap(16),
-                    FormBuilderSwitch(
-                        name: "useInternal",
-                        title: const Text("Use Internal Change")),
+                    Showcase(
+                        key: internalID,
+                        description:
+                            "Check if you want this account to use an internal address for the change like Zashi (ZIP 315)",
+                        child: FormBuilderSwitch(
+                            name: "useInternal",
+                            title: const Text("Use Internal Change"))),
                     Gap(16),
-                    FormBuilderSwitch(
-                        name: "restore",
-                        title: const Text("Restore Account?"),
-                        initialValue: restore,
-                        onChanged: (v) => setState(() => restore = v ?? false)),
+                    Showcase(
+                        key: restoreID,
+                        description:
+                            "Check if you want to restore an existing account",
+                        child: FormBuilderSwitch(
+                            name: "restore",
+                            title: const Text("Restore Account?"),
+                            initialValue: restore,
+                            onChanged: (v) =>
+                                setState(() => restore = v ?? false))),
                     Gap(16),
                     if (restore)
-                      FormBuilderTextField(
-                        name: "key",
-                        decoration: const InputDecoration(
-                            labelText:
-                                "Key (Seed Phrase, Private Key, or Viewing Key)"),
-                        validator: (s) => validKey(s, restore: restore),
-                        initialValue: key,
-                        onChanged: (v) => setState(() => key = v!),
-                      ),
+                      Showcase(
+                          key: keyID,
+                          description:
+                              "Seed phrase (12, 18, 21, 24 words), a Sapling secret key, a viewing key, a unified viewing key, a xpub/xprv transparent key or a BIP-38 key (starting with K or L)",
+                          child: FormBuilderTextField(
+                            name: "key",
+                            decoration: const InputDecoration(
+                                labelText:
+                                    "Key (Seed Phrase, Private Key, or Viewing Key)"),
+                            validator: (s) => validKey(s, restore: restore),
+                            initialValue: key,
+                            onChanged: (v) => setState(() => key = v!),
+                          )),
                     Gap(16),
                     if (restore && isSeed)
+                    Showcase(key: passphraseID, description: "An optional extra word/phrase added to the seed phrase (like in Trezor)", child:
                       FormBuilderTextField(
                         name: "passphrase",
                         decoration: const InputDecoration(
-                            labelText:
-                                "Extra Passphrase (optional)"),
-                      ),
+                            labelText: "Extra Passphrase (optional)"),
+                      )),
                     Gap(16),
                     if (restore && isSeed)
+                    Showcase(key: accountIndexID, description: "The derivation account index. Usually 0, but could be 1, 2, etc if you have additional accounts under the same seed", child:
                       FormBuilderTextField(
                         name: "aindex",
-                        decoration: const InputDecoration(
-                            labelText:
-                                "Account Index"),
+                        decoration:
+                            const InputDecoration(labelText: "Account Index"),
                         keyboardType: TextInputType.number,
                         inputFormatters: [
                           FilteringTextInputFormatter.digitsOnly
                         ],
-                      ),
+                      )),
                     Gap(16),
                     if (restore)
+                      Showcase(key: birthID, description: "Block height when the wallet was created. Save synchronization time by skipping blocks before the birth height", child:
                       FormBuilderTextField(
                         name: "birth",
                         decoration:
@@ -125,7 +181,7 @@ class NewAccountPageState extends State<NewAccountPage> {
                         inputFormatters: [
                           FilteringTextInputFormatter.digitsOnly
                         ],
-                      ),
+                      )),
                   ],
                 ),
               ),
@@ -153,7 +209,9 @@ class NewAccountPageState extends State<NewAccountPage> {
         key: key,
         passphrase: passphrase,
         aindex: int.parse(aindex ?? "0"),
-        birth: birth != null ? int.parse(birth) : AppStoreBase.instance.currentHeight,
+        birth: birth != null
+            ? int.parse(birth)
+            : AppStoreBase.instance.currentHeight,
         useInternal: useInternal ?? false,
       ));
       if (mounted && key.isEmpty) {
