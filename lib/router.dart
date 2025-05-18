03bc18b5ad43628d7b200c061fa290cb5c1409d2
hhanh00 <hanh425@gmail.com>
2025-05-19 04:00:14 +1000

    chore: refactor dkg (#193)

--- lib/router.dart
+++ lib/router.dart
@@ -4,7 +4,6 @@ import 'package:zkool/pages/account.dart';
 import 'package:zkool/pages/accounts.dart';
 import 'package:zkool/pages/disclaimer.dart';
 import 'package:zkool/pages/dkg.dart';
-import 'package:zkool/pages/frost.dart';
 import 'package:zkool/pages/log.dart';
 import 'package:zkool/pages/new_account.dart';
 import 'package:zkool/pages/receive.dart';
@@ -13,7 +12,6 @@ import 'package:zkool/pages/tx.dart';
 import 'package:zkool/pages/tx_view.dart';
 import 'package:zkool/settings.dart';
 import 'package:zkool/src/rust/api/account.dart';
-import 'package:zkool/src/rust/api/frost.dart';
 import 'package:zkool/src/rust/api/pay.dart';
 import 'package:zkool/src/rust/pay.dart';
 import 'package:zkool/widgets/scanner.dart';
@@ -78,10 +76,10 @@ final router = GoRouter(
           return QRPage(text: args["text"], title: args["title"]);
         }),
     GoRoute(path: '/dkg1', builder: (context, state) => DKGPage1()),
-    GoRoute(path: '/dkg2', builder: (context, state) => DKGPage2(state.extra as FrostPackage)),
+    GoRoute(path: '/dkg2', builder: (context, state) => DKGPage2(addresses: state.extra as List<String>)),
     GoRoute(path: '/dkg3', builder: (context, state) => DKGPage3()),
-    GoRoute(path: '/frost1', builder: (context, state) => FrostPage1(state.extra as PcztPackage)),
-    GoRoute(path: '/frost2', builder: (context, state) => FrostPage2()),
+    // GoRoute(path: '/frost1', builder: (context, state) => FrostPage1(state.extra as PcztPackage)),
+    // GoRoute(path: '/frost2', builder: (context, state) => FrostPage2()),
     GoRoute(path: '/settings', builder: (context, state) => SettingsPage()),
     GoRoute(path: '/disclaimer', builder: (context, state) => DisclaimerPage()),
   ],
