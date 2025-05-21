993b16cd1a70ee4c5a010842475979342c304155
Hanh <hanh425@gmail.com>
2025-05-22 02:01:35 +0800

    s

--- misc/codesign-notarize.sh
+++ misc/codesign-notarize.sh
@@ -7,8 +7,8 @@ security unlock-keychain -p $KEYCHAIN_PWD $HOME/build.keychain
 security import /tmp/certs.p12 -k $HOME/build.keychain -P $APPLE_CERTIFICATE_PASSWORD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PWD $HOME/build.keychain
 /usr/bin/codesign --force --entitlements macos/Runner/Release.entitlements -s "$APPLE_CERTIFICATE_NAME" --deep --options runtime build/macos/Build/Products/Release/$APPNAME.app -v
-# Skip notarization
-# xcrun notarytool store-credentials "notarytool-profile" --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD
-# ditto -c -k --keepParent "build/macos/Build/Products/Release/$APPNAME.app" "notarization.zip"
-# xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
-# xcrun stapler staple "build/macos/Build/Products/Release/$APPNAME.app"
+
+xcrun notarytool store-credentials "notarytool-profile" --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD
+ditto -c -k --keepParent "build/macos/Build/Products/Release/$APPNAME.app" "notarization.zip"
+xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
+xcrun stapler staple "build/macos/Build/Products/Release/$APPNAME.app"
