001cb0f2b19182d0229298adf0f0c043377ae7c0
hhanh00 <hanh425@gmail.com>
2025-05-31 18:35:05 +1000

    fix: ios ipa build (#244)

--- misc/codesign-ios.sh
+++ misc/codesign-ios.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-echo "$APPLE_DEV_CERT" | base64 --decode > /tmp/certs.p12
+pushd ios
+pushd certs
+openssl enc -pbkdf2 -aes-256-cbc -salt -d -in zkool3.mobileprovision.enc -out zkool3.mobileprovision -pass pass:$APPLE_DEV_CERT_PWD
+openssl enc -pbkdf2 -aes-256-cbc -salt -d -in apple-distrib.p12.enc -out apple-distrib.p12 -pass pass:$APPLE_DEV_CERT_PWD
+popd
+
 security create-keychain -p $KEYCHAIN_PWD $HOME/build.keychain
 security set-keychain-settings -ut 7200 $HOME/build.keychain
 security default-keychain -s $HOME/build.keychain
 security unlock-keychain -p $KEYCHAIN_PWD $HOME/build.keychain
-security import /tmp/certs.p12 -k $HOME/build.keychain -P $APPLE_DEV_CERT_PWD -T /usr/bin/codesign
+security import certs/apple-distrib.p12 -k $HOME/build.keychain -P $APPLE_DEV_CERT_PWD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PWD $HOME/build.keychain
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$IOS_PROVISION" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/$UUID.mobileprovision
+cp certs/zkool3.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+
+popd
+
+# Request a IOS distribution certificate from the Apple Developer Portal
+# Download the certificate and import it into the keychain
+# Export the certificate and private key as a .p12 file
+# Encode the .p12 file in base64 and set it as the APPLE_DEV_CERT repository secret
+# Request a provisioning profile from the Apple Developer Portal with Apple Store Connect
+# and include the previous certificate
+# Download the provisioning profile and encode it in base64
+# Set the provisioning profile as the IOS_PROVISION repository secret
