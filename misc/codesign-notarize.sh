fac7871ee54ad725620c015ce0efb4a829d04fac
hhanh00 <hanh425@gmail.com>
2025-07-29 23:44:32 +0800

    chore: ios/macos release scripts (#341)

--- misc/codesign-notarize.sh
+++ misc/codesign-notarize.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-echo "$APPLE_CERTIFICATE" | base64 --decode > /tmp/certs.p12
-security create-keychain -p $KEYCHAIN_PWD $HOME/build.keychain
-security set-keychain-settings -ut 7200 $HOME/build.keychain
-security default-keychain -s $HOME/build.keychain
-security unlock-keychain -p $KEYCHAIN_PWD $HOME/build.keychain
-security import /tmp/certs.p12 -k $HOME/build.keychain -P $APPLE_CERTIFICATE_PASSWORD -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PWD $HOME/build.keychain
+pushd misc
 /usr/bin/codesign --force --entitlements macos/Runner/Release.entitlements -s "$APPLE_CERTIFICATE_NAME" --deep --options runtime build/macos/Build/Products/Release/$APPNAME.app -v
 
 xcrun notarytool store-credentials "notarytool-profile" --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD
 ditto -c -k --keepParent "build/macos/Build/Products/Release/$APPNAME.app" "notarization.zip"
 xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
 xcrun stapler staple "build/macos/Build/Products/Release/$APPNAME.app"
+popd
