b213dcfc39fdaea268341f6c0d58cac6b4504410
hhanh00 <hanh425@gmail.com>
2025-07-31 00:57:04 +0800

    fix: macos notarization (#344)

--- misc/codesign-notarize.sh
+++ misc/codesign-notarize.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-pushd misc
 /usr/bin/codesign --force --entitlements macos/Runner/Release.entitlements -s "$APPLE_CERTIFICATE_NAME" --deep --options runtime build/macos/Build/Products/Release/$APPNAME.app -v
 
 xcrun notarytool store-credentials "notarytool-profile" --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD
 ditto -c -k --keepParent "build/macos/Build/Products/Release/$APPNAME.app" "notarization.zip"
 xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
 xcrun stapler staple "build/macos/Build/Products/Release/$APPNAME.app"
-popd
