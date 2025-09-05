58cd0a549097ce1db885efd3a0608074bc815e1a
hhanh00 <hanh425@gmail.com>
2025-09-05 12:47:23 +1000

    feat: create new folders (#472)

--- lib/src/rust/api/account.freezed.dart
+++ lib/src/rust/api/account.freezed.dart
@@ -702,6 +702,148 @@ abstract class _AccountUpdate implements AccountUpdate {
 }
 
 /// @nodoc
+mixin _$Folder {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+
+  /// Create a copy of Folder
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $FolderCopyWith<Folder> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $FolderCopyWith<$Res> {
+  factory $FolderCopyWith(Folder value, $Res Function(Folder) then) =
+      _$FolderCopyWithImpl<$Res, Folder>;
+  @useResult
+  $Res call({int id, String name});
+}
+
+/// @nodoc
+class _$FolderCopyWithImpl<$Res, $Val extends Folder>
+    implements $FolderCopyWith<$Res> {
+  _$FolderCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Folder
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$FolderImplCopyWith<$Res> implements $FolderCopyWith<$Res> {
+  factory _$$FolderImplCopyWith(
+          _$FolderImpl value, $Res Function(_$FolderImpl) then) =
+      __$$FolderImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int id, String name});
+}
+
+/// @nodoc
+class __$$FolderImplCopyWithImpl<$Res>
+    extends _$FolderCopyWithImpl<$Res, _$FolderImpl>
+    implements _$$FolderImplCopyWith<$Res> {
+  __$$FolderImplCopyWithImpl(
+      _$FolderImpl _value, $Res Function(_$FolderImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Folder
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+  }) {
+    return _then(_$FolderImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$FolderImpl implements _Folder {
+  const _$FolderImpl({required this.id, required this.name});
+
+  @override
+  final int id;
+  @override
+  final String name;
+
+  @override
+  String toString() {
+    return 'Folder(id: $id, name: $name)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$FolderImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name);
+
+  /// Create a copy of Folder
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$FolderImplCopyWith<_$FolderImpl> get copyWith =>
+      __$$FolderImplCopyWithImpl<_$FolderImpl>(this, _$identity);
+}
+
+abstract class _Folder implements Folder {
+  const factory _Folder({required final int id, required final String name}) =
+      _$FolderImpl;
+
+  @override
+  int get id;
+  @override
+  String get name;
+
+  /// Create a copy of Folder
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$FolderImplCopyWith<_$FolderImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
 mixin _$FrostParams {
   int get id => throw _privateConstructorUsedError;
   int get n => throw _privateConstructorUsedError;
