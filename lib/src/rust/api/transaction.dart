7301bc908acc07f85c70dd19da1935af050d298a
hhanh00 <hanh425@gmail.com>
2025-09-20 17:48:15 +0800

    feat: fill missing tx prices by querying Coin Gecko (#530)

--- lib/src/rust/api/transaction.dart
+++ lib/src/rust/api/transaction.dart
@@ -6,5 +6,5 @@
 import '../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
-Future<(int?, int?)> getTxWithoutUsdRange() =>
-    RustLib.instance.api.crateApiTransactionGetTxWithoutUsdRange();
+Future<void> fillMissingTxPrices() =>
+    RustLib.instance.api.crateApiTransactionFillMissingTxPrices();
