f8ee170351f6a6d9709039e6277ce9583dc4c79c
hhanh00 <hanh425@gmail.com>
2025-09-23 08:52:35 +0800

    feat: edit tx price on details page (#551)

--- rust/src/frb_generated.rs
+++ rust/src/frb_generated.rs
@@ -38,7 +38,7 @@ flutter_rust_bridge::frb_generated_boilerplate!(
     default_rust_auto_opaque = RustAutoOpaqueMoi,
 );
 pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.11.1";
-pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 62389793;
+pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 830837057;
 
 // Section: executor
 
@@ -3228,6 +3228,44 @@ fn wire__crate__api__transaction__set_tx_category_impl(
         },
     )
 }
+fn wire__crate__api__transaction__set_tx_price_impl(
+    port_: flutter_rust_bridge::for_generated::MessagePort,
+    ptr_: flutter_rust_bridge::for_generated::PlatformGeneralizedUint8ListPtr,
+    rust_vec_len_: i32,
+    data_len_: i32,
+) {
+    FLUTTER_RUST_BRIDGE_HANDLER.wrap_async::<flutter_rust_bridge::for_generated::SseCodec, _, _, _>(
+        flutter_rust_bridge::for_generated::TaskInfo {
+            debug_name: "set_tx_price",
+            port: Some(port_),
+            mode: flutter_rust_bridge::for_generated::FfiCallMode::Normal,
+        },
+        move || {
+            let message = unsafe {
+                flutter_rust_bridge::for_generated::Dart2RustMessageSse::from_wire(
+                    ptr_,
+                    rust_vec_len_,
+                    data_len_,
+                )
+            };
+            let mut deserializer =
+                flutter_rust_bridge::for_generated::SseDeserializer::new(message);
+            let api_id = <u32>::sse_decode(&mut deserializer);
+            let api_price = <Option<f64>>::sse_decode(&mut deserializer);
+            deserializer.end();
+            move |context| async move {
+                transform_result_sse::<_, flutter_rust_bridge::for_generated::anyhow::Error>(
+                    (move || async move {
+                        let output_ok =
+                            crate::api::transaction::set_tx_price(api_id, api_price).await?;
+                        Ok(output_ok)
+                    })()
+                    .await,
+                )
+            }
+        },
+    )
+}
 fn wire__crate__api__network__set_use_tor_impl(
     ptr_: flutter_rust_bridge::for_generated::PlatformGeneralizedUint8ListPtr,
     rust_vec_len_: i32,
@@ -4895,12 +4933,13 @@ fn pde_ffi_dispatcher_primary_impl(
         88 => {
             wire__crate__api__transaction__set_tx_category_impl(port, ptr, rust_vec_len, data_len)
         }
-        90 => wire__crate__api__pay__sign_transaction_impl(port, ptr, rust_vec_len, data_len),
-        91 => wire__crate__api__pay__store_pending_tx_impl(port, ptr, rust_vec_len, data_len),
-        92 => wire__crate__api__sync__synchronize_impl(port, ptr, rust_vec_len, data_len),
-        94 => wire__crate__api__account__transparent_sweep_impl(port, ptr, rust_vec_len, data_len),
-        96 => wire__crate__api__pay__unpack_transaction_impl(port, ptr, rust_vec_len, data_len),
-        97 => wire__crate__api__account__update_account_impl(port, ptr, rust_vec_len, data_len),
+        89 => wire__crate__api__transaction__set_tx_price_impl(port, ptr, rust_vec_len, data_len),
+        91 => wire__crate__api__pay__sign_transaction_impl(port, ptr, rust_vec_len, data_len),
+        92 => wire__crate__api__pay__store_pending_tx_impl(port, ptr, rust_vec_len, data_len),
+        93 => wire__crate__api__sync__synchronize_impl(port, ptr, rust_vec_len, data_len),
+        95 => wire__crate__api__account__transparent_sweep_impl(port, ptr, rust_vec_len, data_len),
+        97 => wire__crate__api__pay__unpack_transaction_impl(port, ptr, rust_vec_len, data_len),
+        98 => wire__crate__api__account__update_account_impl(port, ptr, rust_vec_len, data_len),
         _ => unreachable!(),
     }
 }
@@ -4926,9 +4965,9 @@ fn pde_ffi_dispatcher_sync_impl(
         74 => wire__crate__api__account__receivers_from_ua_impl(ptr, rust_vec_len, data_len),
         86 => wire__crate__api__init__set_log_stream_impl(ptr, rust_vec_len, data_len),
         87 => wire__crate__api__network__set_lwd_impl(ptr, rust_vec_len, data_len),
-        89 => wire__crate__api__network__set_use_tor_impl(ptr, rust_vec_len, data_len),
-        93 => wire__crate__api__pay__to_plan_impl(ptr, rust_vec_len, data_len),
-        95 => wire__crate__api__account__ua_from_ufvk_impl(ptr, rust_vec_len, data_len),
+        90 => wire__crate__api__network__set_use_tor_impl(ptr, rust_vec_len, data_len),
+        94 => wire__crate__api__pay__to_plan_impl(ptr, rust_vec_len, data_len),
+        96 => wire__crate__api__account__ua_from_ufvk_impl(ptr, rust_vec_len, data_len),
         _ => unreachable!(),
     }
 }
