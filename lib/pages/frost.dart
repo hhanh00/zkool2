867b5d3c0a8bd15094b55921e123302ddb308a39
hhanh00 <hanh425@gmail.com>
2025-12-30 07:20:28 +0800

    fix: use helper fn to ensure that data is loaded (#710)

--- lib/pages/frost.dart
+++ lib/pages/frost.dart
@@ -28,54 +28,52 @@ class FrostPage1 extends ConsumerStatefulWidget {
   ConsumerState<FrostPage1> createState() => FrostPage1State();
 }
 
-Widget buildFrostPage(BuildContext context, WidgetRef ref,
-    {required int index, required bool finished, required Widget child,}) {
+Widget buildFrostPage(
+  BuildContext context,
+  WidgetRef ref, {
+  required int index,
+  required bool finished,
+  required Widget child,
+}) {
   return Scaffold(
-      appBar:
-          AppBar(title: const Text("Frost Multi Party Signature"), actions: [
+    appBar: AppBar(
+      title: const Text("Frost Multi Party Signature"),
+      actions: [
         (finished)
             ? IconButton(
                 onPressed: () {
                   GoRouter.of(context).go("/");
                 },
-                icon: Icon(Icons.close),)
+                icon: Icon(Icons.close),
+              )
             : IconButton(
                 onPressed: () => onCancel(context, ref),
-                icon: const Icon(Icons.cancel),),
-      ],),
-      body: CustomScrollView(slivers: [
+                icon: const Icon(Icons.cancel),
+              ),
+      ],
+    ),
+    body: CustomScrollView(
+      slivers: [
         PinnedHeaderSliver(child: FrostSteps(currentIndex: index)),
         SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            sliver: SliverToBoxAdapter(child: child),),
-      ],),);
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          sliver: SliverToBoxAdapter(child: child),
+        ),
+      ],
+    ),
+  );
 }
 
 class FrostPage1State extends ConsumerState<FrostPage1> {
   late final c = ref.read(coinContextProvider);
   final formKey = GlobalKey<FormBuilderState>();
   List<Account> accounts = [];
+  AccountData? account;
   FrostParams? frostParams;
 
   @override
   void initState() {
     super.initState();
-    Future(() async {
-      final as = await ref.read(getAccountsProvider.future);
-      accounts = as.where((e) => !e.hidden).toList();
-      final selectedAccount = ref.read(selectedAccountProvider).requireValue!;
-      final account = await ref.read(accountProvider(selectedAccount.id).future);
-      frostParams = account.frostParams;
-      final signing = await isSigningInProgress(c: c);
-      if (signing) {
-        if (context.mounted) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            GoRouter.of(context).pushReplacement("/frost2");
-          });
-        }
-      }
-      setState(() {});
-    });
   }
 
   void tutorial() async {
@@ -84,7 +82,32 @@ class FrostPage1State extends ConsumerState<FrostPage1> {
 
   @override
   Widget build(BuildContext context) {
-    if (frostParams == null) return blank(context);
+    try {
+      final accountsAV = ref.watch(getAccountsProvider);
+      ensureAV(context, accountsAV);
+      accounts = accountsAV.requireValue.where((e) => !e.hidden).toList();
+
+      final accountAV = ref.watch(getCurrentAccountProvider);
+      ensureAV(context, accountAV);
+      account = accountAV.requireValue!;
+    } on Widget catch (w) {
+      return w;
+    }
+
+    if (frostParams == null) {
+      frostParams = account!.frostParams;
+      Future(() async {
+        final signing = await isSigningInProgress(c: c);
+        if (signing) {
+          if (context.mounted) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              GoRouter.of(context).pushReplacement("/frost2");
+            });
+          }
+        }
+        setState(() {});
+      });
+    }
 
     final pinlock = ref.watch(lifecycleProvider);
     if (pinlock.value == true) return PinLock();
@@ -92,58 +115,68 @@ class FrostPage1State extends ConsumerState<FrostPage1> {
     Future(tutorial);
 
     return Scaffold(
-        appBar: AppBar(title: const Text("Frost Multi Party Signature")),
-        body: FormBuilder(
-            key: formKey,
-            child: Column(children: [
-              ListTile(
-                  title: Text("Your Participant ID"),
-                  subtitle: Text(frostParams!.id.toString()),
+      appBar: AppBar(title: const Text("Frost Multi Party Signature")),
+      body: FormBuilder(
+        key: formKey,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("Your Participant ID"),
+              subtitle: Text(frostParams!.id.toString()),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Showcase(
+                key: coordinatorID,
+                description: "Participant ID who is coordinating the multisignature",
+                child: FormBuilderDropdown(
+                  name: "coordinator",
+                  decoration: const InputDecoration(
+                    labelText: "ID of the coordinator",
+                  ),
+                  initialValue: 1,
+                  items: List.generate(
+                    5,
+                    (i) => DropdownMenuItem(
+                      value: i + 1,
+                      child: Text("${i + 1}"),
+                    ),
+                  ),
                 ),
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Showcase(
-                      key: coordinatorID,
-                      description:
-                          "Participant ID who is coordinating the multisignature",
-                      child: FormBuilderDropdown(
-                          name: "coordinator",
-                          decoration: const InputDecoration(
-                            labelText: "ID of the coordinator",
-                          ),
-                          initialValue: 1,
-                          items: List.generate(
-                            5,
-                            (i) => DropdownMenuItem(
-                              value: i + 1,
-                              child: Text("${i + 1}"),
-                            ),
-                          ),),),),
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Showcase(
-                      key: fundingID2,
-                      description:
-                          "Multisig uses messages in memos. The process needs a ~0.0001 ZEC to pay for the fees. This account is used to pay for them.",
-                      child: FormBuilderDropdown(
-                        name: "account",
-                        decoration: const InputDecoration(
-                            labelText:
-                                "Funding Account for the FROST messages",),
-                        items: accounts
-                            .map((a) => DropdownMenuItem(
-                                  value: a.id,
-                                  child: Text(a.name),
-                                ),)
-                            .toList(),
-                        validator: FormBuilderValidators.required(),
-                      ),),),
-              Gap(16),
-              ElevatedButton.icon(
-                  onPressed: onNext,
-                  label: Text("Next"),
-                  icon: Icon(Icons.arrow_forward),),
-            ],),),);
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Showcase(
+                key: fundingID2,
+                description: "Multisig uses messages in memos. The process needs a ~0.0001 ZEC to pay for the fees. This account is used to pay for them.",
+                child: FormBuilderDropdown(
+                  name: "account",
+                  decoration: const InputDecoration(
+                    labelText: "Funding Account for the FROST messages",
+                  ),
+                  items: accounts
+                      .map(
+                        (a) => DropdownMenuItem(
+                          value: a.id,
+                          child: Text(a.name),
+                        ),
+                      )
+                      .toList(),
+                  validator: FormBuilderValidators.required(),
+                ),
+              ),
+            ),
+            Gap(16),
+            ElevatedButton.icon(
+              onPressed: onNext,
+              label: Text("Next"),
+              icon: Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   void onNext() async {
@@ -151,11 +184,7 @@ class FrostPage1State extends ConsumerState<FrostPage1> {
     if (form.saveAndValidate()) {
       final coordinator = form.fields["coordinator"]!.value as int;
       final fundingAccount = form.fields["account"]!.value as int;
-      await initSign(
-          pczt: widget.pczt,
-          coordinator: coordinator,
-          fundingAccount: fundingAccount,
-          c: c);
+      await initSign(pczt: widget.pczt, coordinator: coordinator, fundingAccount: fundingAccount, c: c);
       if (!mounted) return;
       await GoRouter.of(context).pushReplacement("/frost2");
     }
@@ -194,10 +223,13 @@ class FrostPage2State extends ConsumerState<FrostPage2> {
   @override
   Widget build(BuildContext context) {
     final t = Theme.of(context).textTheme;
-    return buildFrostPage(context, ref,
-        index: currentIndex,
-        finished: finished,
-        child: Column(children: [Text(message, style: t.bodyLarge)]),);
+    return buildFrostPage(
+      context,
+      ref,
+      index: currentIndex,
+      finished: finished,
+      child: Column(children: [Text(message, style: t.bodyLarge)]),
+    );
   }
 
   int? currentHeight;
@@ -207,73 +239,73 @@ class FrostPage2State extends ConsumerState<FrostPage2> {
     if (currentHeight != null && currentHeight == h) return;
     currentHeight = h;
     final as = await ref.read(getAccountsProvider.future);
-    final accounts = as
-        .where((e) => e.enabled)
-        .toList();
+    final accounts = as.where((e) => e.enabled).toList();
     final synchronizer = ref.read(synchronizerProvider.notifier);
     await synchronizer.startSynchronize(accounts);
 
     final status = doSign(c: c);
-    status.listen((s) {
-      if (s is SigningStatus_WaitingForCommitments) {
-        setState(() {
-          message = "Waiting for other participants to send their commitments";
-          currentIndex = 1; // coordinator
-        });
-      } else if (s is SigningStatus_SendingCommitment) {
-        setState(() {
-          message = "Sending our commitments to the coordinator";
-          currentIndex = 1; // other
-        });
-      } else if (s is SigningStatus_SendingSigningPackage) {
-        setState(() {
-          message = "Broadcasting the signing package to all participants";
-          currentIndex = 2; // coordinator
-        });
-      } else if (s is SigningStatus_WaitingForSigningPackage) {
-        setState(() {
-          message = "Waiting for the signing package from the coordinator";
-          currentIndex = 2; // other
-        });
-      } else if (s is SigningStatus_SendingSignatureShare) {
-        setState(() {
-          message = "Sending our signature share to the coordinator";
-          currentIndex = 3; // other
-        });
-      } else if (s is SigningStatus_SigningCompleted) {
-        setState(() {
-          message = "Signing completed";
-          currentIndex = 3; // other
-          finished = true;
-        });
-      } else if (s is SigningStatus_WaitingForSignatureShares) {
-        setState(() {
-          message =
-              "Waiting for the signature share from the other participants";
-          currentIndex = 2; // coordinator
-        });
-      } else if (s is SigningStatus_PreparingTransaction) {
-        setState(() {
-          message = "Assembling the transaction";
-          currentIndex = 3; // coordinator
-        });
-      } else if (s is SigningStatus_SendingTransaction) {
-        setState(() {
-          message = "Sending the transaction to the network";
-          currentIndex = 3; // coordinator
-        });
-      } else if (s is SigningStatus_TransactionSent) {
-        setState(() {
-          message = "TX ID: ${s.field0}";
-          currentIndex = 3; // coordinator
-          finished = true;
-        });
-      }
-    }, onError: (e) async {
-      final exc = e as AnyhowException;
-      if (!context.mounted) return;
-      await showException(context, exc.message);
-    },);
+    status.listen(
+      (s) {
+        if (s is SigningStatus_WaitingForCommitments) {
+          setState(() {
+            message = "Waiting for other participants to send their commitments";
+            currentIndex = 1; // coordinator
+          });
+        } else if (s is SigningStatus_SendingCommitment) {
+          setState(() {
+            message = "Sending our commitments to the coordinator";
+            currentIndex = 1; // other
+          });
+        } else if (s is SigningStatus_SendingSigningPackage) {
+          setState(() {
+            message = "Broadcasting the signing package to all participants";
+            currentIndex = 2; // coordinator
+          });
+        } else if (s is SigningStatus_WaitingForSigningPackage) {
+          setState(() {
+            message = "Waiting for the signing package from the coordinator";
+            currentIndex = 2; // other
+          });
+        } else if (s is SigningStatus_SendingSignatureShare) {
+          setState(() {
+            message = "Sending our signature share to the coordinator";
+            currentIndex = 3; // other
+          });
+        } else if (s is SigningStatus_SigningCompleted) {
+          setState(() {
+            message = "Signing completed";
+            currentIndex = 3; // other
+            finished = true;
+          });
+        } else if (s is SigningStatus_WaitingForSignatureShares) {
+          setState(() {
+            message = "Waiting for the signature share from the other participants";
+            currentIndex = 2; // coordinator
+          });
+        } else if (s is SigningStatus_PreparingTransaction) {
+          setState(() {
+            message = "Assembling the transaction";
+            currentIndex = 3; // coordinator
+          });
+        } else if (s is SigningStatus_SendingTransaction) {
+          setState(() {
+            message = "Sending the transaction to the network";
+            currentIndex = 3; // coordinator
+          });
+        } else if (s is SigningStatus_TransactionSent) {
+          setState(() {
+            message = "TX ID: ${s.field0}";
+            currentIndex = 3; // coordinator
+            finished = true;
+          });
+        }
+      },
+      onError: (e) async {
+        final exc = e as AnyhowException;
+        if (!context.mounted) return;
+        await showException(context, exc.message);
+      },
+    );
   }
 }
 
@@ -313,9 +345,11 @@ class FrostSteps extends StatelessWidget {
 
 void onCancel(BuildContext context, WidgetRef ref) async {
   final c = ref.read(coinContextProvider);
-  final confirmed = await confirmDialog(context,
-      title: "Cancel Multi Signature",
-      message: "Are you sure you want to cancel the multi signature process?",);
+  final confirmed = await confirmDialog(
+    context,
+    title: "Cancel Multi Signature",
+    message: "Are you sure you want to cancel the multi signature process?",
+  );
   if (!confirmed) return;
   await resetSign(c: c);
   if (!context.mounted) return;
