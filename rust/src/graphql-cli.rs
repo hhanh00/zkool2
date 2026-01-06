3a492735b2c284e795cf3bf243860d7ddef1123e
hhanh00 <hanh425@gmail.com>
2026-01-06 22:18:25 +0800

    feat: add polling interval to coin config (#727)

--- rust/src/graphql-cli.rs
+++ rust/src/graphql-cli.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
-use figment::Figment;
 use figment::providers::{Format, Toml};
+use figment::Figment;
 use juniper::{EmptySubscription, RootNode};
 use rlz::api::coin::Coin;
 use rlz::graphql::mutation::run_mempool;
@@ -10,15 +10,26 @@ type Schema = RootNode<Query, Mutation, EmptySubscription<Context>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    rustls::crypto::ring::default_provider().install_default().unwrap();
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .unwrap();
+    let subscriber = tracing_subscriber::fmt()
+        .with_ansi(false)
+        .compact()
+        .finish();
+    let _ = tracing::subscriber::set_global_default(subscriber);
     let config = Figment::new().merge(Toml::file("zkool.toml"));
-    let db_path: String = config.extract_inner("custom.db_path")?;
-    let lwd_url: String = config.extract_inner("custom.lwd_url")?;
+    let db_path: String = config.extract_inner("db_path")?;
+    let lwd_url: String = config.extract_inner("lwd_url")?;
+    let polling_interval: u32 = config.extract_inner("polling_interval")?;
+    tracing::info!("db_path {db_path} lwd_url {lwd_url} polling_interval {polling_interval}");
     let coin = Coin::new()
         .open_database(db_path, None)
         .await?
         .set_lwd(0, lwd_url)?;
+
     let context = Context::new(coin);
+
     tokio::spawn(run_mempool(context.clone()));
 
     Ok(())
