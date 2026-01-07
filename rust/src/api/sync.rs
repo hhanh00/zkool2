370f37524dc9f4a204b6399909df1dbedfc1041f
hhanh00 <hanh425@gmail.com>
2026-01-07 09:50:45 +0800

    feat: "synchronize" returns current height (#731)

--- rust/src/api/sync.rs
+++ rust/src/api/sync.rs
@@ -19,7 +19,7 @@ pub async fn synchronize(
     transparent_limit: u32,
     checkpoint_age: u32,
     c: &Coin,
-) -> Result<()> {
+) -> Result<u32> {
     crate::sync::synchronize_impl(
         progress,
         accounts,
