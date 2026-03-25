10e86a8ce94e47a9cc3f83d92d91d581b73f4b22
Hanh Huynh Huu <hanh425@gmail.com>
2026-03-25 16:42:36 +0800

    chore: smoke tests

---
+++ example/sh/smoke.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+GRAPHQL_URL="http://localhost:8000/graphql"
+
+echo "Test transfer to new wallet"
+
+echo "Import funded wallet"
+WALLET=$(gq $GRAPHQL_URL \
+  -q 'mutation ($main: String!) {
+  createAccount(newAccount: {
+    name: "Main"
+    key: $main
+    aindex: 0
+    useInternal: false
+    birth: 1
+  })
+}' -v "main=$SEED" | jq -r '.data.createAccount')
+
+echo "Synchronize"
+gq $GRAPHQL_URL \
+-q 'mutation ($account: Int!) {
+  synchronizeAccount(idAccount: $account)
+}' -v "account=$WALLET" | jq -r '.data.synchronizeAccount'
+
+echo "Create new wallet"
+A2=$(gq $GRAPHQL_URL \
+  -q 'mutation {
+  createAccount(newAccount: {
+    name: "A"
+    key: ""
+    aindex: 0
+    useInternal: false
+    birth: 1
+  })
+}' | jq -r '.data.createAccount')
+
+echo "Get new wallet address"
+ADDRESS=$(gq $GRAPHQL_URL \
+  -q 'query ($account: Int!) {
+  addressByAccount(idAccount: $account) {
+    orchard
+  }
+}' -v "account=$A2" | jq -r '.data.addressByAccount.orchard')
+
+echo "Synchronize"
+gq $GRAPHQL_URL \
+-q 'mutation ($accounts: [Int!]!) {
+  synchronize(idAccounts: $accounts)
+}' -v "accounts=[$WALLET,$A2]" | jq -r '.data.synchronize'
+
+echo "Show funding balance"
+gq $GRAPHQL_URL \
+-q 'query ($account: Int!) {
+  balanceByAccount(idAccount: $account) {
+    orchard
+  }
+}' -v "account=$WALLET" | jq -r '.data.balanceByAccount.orchard'
+
+echo "Send funds"
+TXID=$(gq $GRAPHQL_URL \
+-q 'mutation ($account: Int!, $address: String!, $amount: BigDecimal!){
+  pay(idAccount: $account
+  payment:  {
+    recipients:  [{
+      address: $address
+      amount: $amount
+    }]
+  })
+}' -v "account=$WALLET" -v "address=$ADDRESS" \
+-v 'amount=10.5' | jq -r '.data.pay')
+
+echo "Pause for 30s"
+sleep 30
+
+echo "Synchronize"
+gq $GRAPHQL_URL \
+-q 'mutation ($account: Int!) {
+  synchronizeAccount(idAccount: $account)
+}' -v "account=$A2" | jq -r '.data.synchronizeAccount'
+
+echo "Show new wallet balance"
+FINAL_BAL=$(gq $GRAPHQL_URL \
+-q 'query ($account: Int!) {
+  balanceByAccount(idAccount: $account) {
+    orchard
+  }
+}' -v "account=$A2" | jq -r '.data.balanceByAccount.orchard')
+
+echo $FINAL_BAL
+
+if [ "$FINAL_BAL" != "10.50000000" ]; then
+    echo "Error: expected 10.50000000, got $FINAL_BAL" >&2
+    exit 1
+fi
