54774ddddffe9af308340f134c0020ffecff8aaf
hhanh00 <hanh425@gmail.com>
2026-04-16 22:07:41 +0800

    fix: add timestamp to vault log entry (#921)

--- rust/src/vault/mod.rs
+++ rust/src/vault/mod.rs
@@ -50,6 +50,7 @@ impl<IO: VaultIO> Vault<IO> {
 
     pub async fn store_account(
         &self,
+        timestamp: u32,
         name: String,
         seed: String,
         aindex: u32,
@@ -64,7 +65,7 @@ impl<IO: VaultIO> Vault<IO> {
 
         let xpk = x25519_dalek::PublicKey::from(<[u8; 32]>::try_from(pk).map_err(|_| anyhow::anyhow!("Invalid pk length"))?);
 
-        let account = crypto::AccountPayload { name, entropy, aindex, use_internal: _use_internal, birth_height };
+        let account = crypto::AccountPayload { timestamp, name, entropy, aindex, use_internal: _use_internal, birth_height };
         let entry_bytes = crypto::encrypt_account(account, xpk)?;
 
         self.io_handler.append(entry_bytes).await?;
