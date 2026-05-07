e1d056f5865be1af256da6f7eb5f0e94cb5a7358
hhanh00 <hanh425@gmail.com>
2026-05-08 02:08:18 +0800

    feat: support uncompressed transparent private keys (5XXX) (#973)

--- rust/src/bip38.rs
+++ rust/src/bip38.rs
@@ -1,17 +1,23 @@
 use anyhow::Result;
 use secp256k1::SecretKey;
 
-pub fn export_tsk(sk: &SecretKey) -> String {
-    let mut v = sk.secret_bytes().to_vec();
-    v.push(0x01);
+pub struct TSecretKey(pub SecretKey, pub bool);
+
+pub fn export_tsk(sk: &TSecretKey) -> String {
+    let mut v = sk.0.secret_bytes().to_vec();
+    if !sk.1 {
+        v.push(0x01);
+    }
     bs58::encode(v).with_check_version(0x80).into_string()
 }
 
-pub fn import_tsk(tsk: &str) -> Result<SecretKey> {
+pub fn import_tsk(tsk: &str) -> Result<TSecretKey> {
     let v = bs58::decode(tsk).with_check(Some(0x80)).into_vec()?;
-    if v.len() != 34 {
-        return Err(anyhow::anyhow!("Invalid TSK length"));
+    tracing::info!("{}", hex::encode(&v));
+    if v.len() != 33 && v.len() != 34 {
+        return Err(anyhow::anyhow!("Invalid TSK length {}", v.len()));
     }
     let sk = SecretKey::from_slice(&v[1..33]).unwrap();
-    Ok(sk)
+    let compressed = v.len() == 34 && v[33] == 0x01;
+    Ok(TSecretKey(sk, !compressed))
 }
