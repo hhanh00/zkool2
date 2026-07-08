398cbdf1ef4deda2276a1bbfdf7ffb91d032c0ad
Hanh Huynh Huu <hanh425@gmail.com>
2026-07-08 18:09:38 +0800

    fix: comment out proto build in build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,12 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure()
-        .out_dir("src/")
-        .compile_protos(&["../protos/service.proto"], &["../protos/"])?;
-    // tonic_prost_build names the output after the proto package
-    // (cash.z.wallet.sdk.rpc). Rename to the module name lib.rs expects.
-    std::fs::rename(
-        "src/cash.z.wallet.sdk.rpc.rs",
-        "src/lwd.rs",
-    )?;
+    // tonic_prost_build::configure()
+    //     .out_dir("src/")
+    //     .compile_protos(&["../protos/service.proto"], &["../protos/"])?;
+    // std::fs::rename(
+    //     "src/cash.z.wallet.sdk.rpc.rs",
+    //     "src/lwd.rs",
+    // )?;
     Ok(())
 }
